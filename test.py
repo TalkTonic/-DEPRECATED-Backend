d362cc6c8a07eee67ad51782e5de11b270b38995
kynes446 <mattlee446@gmail.com>
2015-09-05 09:42:50 -0400

    start of rest api for conversation login successful

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/conversations')
-def get_conversations():
-    return 'Hello World!'
+@app.route('/conversations/<username>/<password>')
+def get_conversations(username,password):
+    return username + "pass" +  password
 
 if __name__ == '__main__':
     app.run()
